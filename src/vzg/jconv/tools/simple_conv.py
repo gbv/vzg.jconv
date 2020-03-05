383e64eca0ed974b074e7b95bd7197a55874e0e8
Marc-J. Tegethoff <marc.tegethoff@gbv.de>
2020-03-05 20:02:02 +0100

    unique filename

--- src/vzg/jconv/tools/simple_conv.py
+++ src/vzg/jconv/tools/simple_conv.py
@@ -63,8 +63,7 @@ def fromarchive(options):
 
                             jsonarchive.writestr(apath.as_posix(),
                                                  article.json,
-                                                 compress_type=zipfile.ZIP_DEFLATED,
-                                                 compresslevel=6)
+                                                 compress_type=zipfile.ZIP_DEFLATED)
 
                     if options.stop and jconv.validation_failed:
                         msg = "Validation problem"
@@ -134,6 +133,59 @@ def jats(options):
                 return None
 
 
+def springer(options):
+    """Use a ZIP Archive as source."""
+    import uuid
+
+    logger = logging.getLogger(__name__)
+
+    jpath = Path(options.jfiles[0]).absolute()
+    opath = Path(options.outdir).absolute()
+    dst = opath / jpath.name
+    deliverysignature = uuid.uuid4()
+
+    if not opath.exists():
+        opath.mkdir(0o755, parents=True)
+
+    with zipfile.ZipFile(dst, 'w') as jsonarchive:
+        with zipfile.ZipFile(jpath) as xmlarchive:
+            num_xml = 0
+            for name in xmlarchive.namelist():
+                num_xml += 1
+
+            num_xml = float(num_xml)
+
+            for i, zipinfo in enumerate(xmlarchive.infolist()):
+                with tempfile.NamedTemporaryFile("w+b") as tmpfh:
+                    tmpfh.write(xmlarchive.read(zipinfo))
+                    tmpfh.flush()
+
+                    jatspath = Path(tmpfh.name)
+
+                    xpercent = i / num_xml * 100
+                    msg = f"{zipinfo.filename} ({xpercent:.2f}%)"
+                    logger.info(msg)
+
+                    jconv = JatsConverter(jatspath, validate=options.validate)
+                    jconv.run()
+
+                    anum = len(jconv.articles)
+                    msg = f"\t{anum} article(s)"
+                    logger.info(msg)
+
+                    if options.dry_run is False:
+                        for j, article in enumerate(jconv.articles):
+                            aname = f"{deliverysignature}-{i}-{j}.json"
+                            jsonarchive.writestr(aname,
+                                                 article.json,
+                                                 compress_type=zipfile.ZIP_DEFLATED)
+
+                    if options.stop and jconv.validation_failed:
+                        msg = "Validation problem"
+                        logger.info(msg)
+                        break
+
+
 def run():
     """Start the application"""
     from argparse import ArgumentParser
@@ -144,50 +196,88 @@ def run():
 
     subparsers = parser.add_subparsers()
 
-    parser_jats = subparsers.add_parser('jats',
-                                        help='Convert JATS files')
+    # parser_jats = subparsers.add_parser('jats',
+    #                                     help='Convert JATS files')
+    #
+    # parser_jats.add_argument("-n",
+    #                          "--dry-run",
+    #                          dest='dry_run',
+    #                          action='store_true',
+    #                          default=False,
+    #                          help='Do nothing')
+    #
+    # parser_jats.add_argument("-o",
+    #                          "--output-directory",
+    #                          dest="outdir",
+    #                          metavar='Output directory',
+    #                          type=str,
+    #                          default="output",
+    #                          help='Directory of JSON files')
+    #
+    # parser_jats.add_argument("--stop",
+    #                          dest='stop',
+    #                          action='store_true',
+    #                          default=False,
+    #                          help='Stop if JSON Schema Validation fails')
+    #
+    # parser_jats.add_argument("--validate",
+    #                          dest='validate',
+    #                          action='store_true',
+    #                          default=False,
+    #                          help='JSON Schema Validation')
+    #
+    # parser_jats.add_argument(dest="jfiles",
+    #                          metavar='Directory / ZIP-File',
+    #                          type=str,
+    #                          nargs=1,
+    #                          help='Directory or archive file of JATS files')
+    #
+    # parser_jats.set_defaults(func=jats)
 
-    parser_jats.add_argument("--logfile",
-                             default="",
-                             dest="logfile",
-                             metavar='Logfile',
-                             type=str,
-                             nargs="?")
+    parser_springer = subparsers.add_parser('springer',
+                                            help='Convert JATS files from Springer ZIP files')
 
-    parser_jats.add_argument("-n",
-                             "--dry-run",
-                             dest='dry_run',
-                             action='store_true',
-                             default=False,
-                             help='Do nothing')
+    parser_springer.add_argument("-n",
+                                 "--dry-run",
+                                 dest='dry_run',
+                                 action='store_true',
+                                 default=False,
+                                 help='Do nothing')
 
-    parser_jats.add_argument("-o",
-                             "--output-directory",
-                             dest="outdir",
-                             metavar='Output directory',
-                             type=str,
-                             default="output",
-                             help='Directory of JSON files')
+    parser_springer.add_argument("-o",
+                                 "--output-directory",
+                                 dest="outdir",
+                                 metavar='Output directory',
+                                 type=str,
+                                 default="output",
+                                 help='Directory of JSON files')
 
-    parser_jats.add_argument("--stop",
-                             dest='stop',
-                             action='store_true',
-                             default=False,
-                             help='Stop if JSON Schema Validation fails')
+    parser_springer.add_argument("--stop",
+                                 dest='stop',
+                                 action='store_true',
+                                 default=False,
+                                 help='Stop if JSON Schema Validation fails')
 
-    parser_jats.add_argument("--validate",
-                             dest='validate',
-                             action='store_true',
-                             default=False,
-                             help='JSON Schema Validation')
+    parser_springer.add_argument("--validate",
+                                 dest='validate',
+                                 action='store_true',
+                                 default=False,
+                                 help='JSON Schema Validation')
 
-    parser_jats.add_argument(dest="jfiles",
-                             metavar='Directory / ZIP-File',
-                             type=str,
-                             nargs=1,
-                             help='Directory or archive file of JATS files')
+    parser_springer.add_argument(dest="jfiles",
+                                 metavar='ZIP-File',
+                                 type=str,
+                                 nargs=1,
+                                 help='ZIP file with JATS files')
 
-    parser_jats.set_defaults(func=jats)
+    parser_springer.set_defaults(func=springer)
+
+    parser.add_argument("--logfile",
+                        default="",
+                        dest="logfile",
+                        metavar='Logfile',
+                        type=str,
+                        nargs="?")
 
     parser.add_argument("-v",
                         "--verbose",
