8e7c9f978766c1a288f6d2ec5fd7b386c5c822b1
Marc-J. Tegethoff <marc.tegethoff@gbv.de>
2023-02-15 18:49:48 +0100

    getPersonName function removed

--- src/vzg/jconv/utils/__init__.py
+++ src/vzg/jconv/utils/__init__.py
@@ -3,7 +3,7 @@
 
 ##############################################################################
 #
-# Copyright (c) 2020 Verbundzentrale des GBV.
+# Copyright (c) 2020-2023 Verbundzentrale des GBV.
 # All Rights Reserved.
 #
 ##############################################################################
@@ -13,7 +13,6 @@
 from lxml import etree
 import re
 from vzg.jconv.gapi import NAMESPACES
-import logging
 
 __author__ = """Marc-J. Tegethoff <marc.tegethoff@gbv.de>"""
 __docformat__ = "plaintext"
@@ -88,44 +87,3 @@ def node2text(node: etree._Element) -> str:
         nodetext = nodetext.replace(c_, "")
 
     return nodetext
-
-
-def getPerson(node: etree._Element) -> dict:
-    """Extract a person"""
-    logger = logging.getLogger(__name__)
-
-    person = {"firstname": "", "lastname": "", "fullname": ""}
-
-    name_node = None
-
-    if isinstance(node.find("name"), etree._Element):
-        name_node = node.find("name")
-    elif isinstance(node.find("name-alternatives"), etree._Element):
-        name_node = node.xpath("name-alternatives/name")[0]
-
-    if name_node is None:
-        return None
-
-    try:
-        person["firstname"] = name_node.xpath("given-names/text()")[0].strip()
-        person["fullname"] = person["firstname"]
-    except IndexError:
-        msg = "no firstname"
-        logger.info(msg)
-
-    try:
-        person["lastname"] = name_node.xpath("surname/text()")[0].strip()
-        person["fullname"] += f""" {person["lastname"]}"""
-    except IndexError:
-        msg = "no lastname"
-        logger.info(msg)
-
-    logger.debug(person)
-
-    for key, value in person.items():
-        if len(value) == 0:
-            msg = f"no {key}"
-            logger.info(msg)
-            return None
-
-    return person
