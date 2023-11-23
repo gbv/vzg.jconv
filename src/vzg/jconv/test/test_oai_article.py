8fb0b1960ef1212a5e85a5f94b0d69ccae15b555
Marc-J. Tegethoff <marc.tegethoff@gbv.de>
2023-11-23 18:41:22 +0100

    More article tests

--- src/vzg/jconv/test/test_oai_article.py
+++ src/vzg/jconv/test/test_oai_article.py
@@ -10,6 +10,7 @@
 """
 
 import datetime
+import json
 import unittest
 from pathlib import Path
 from zope.interface import providedBy
@@ -26,11 +27,13 @@ class Openedition(unittest.TestCase):
 
     def setUp(self) -> None:
         self.baseurl = Path(
-            "data/tests/oai/response.xml").absolute().as_posix()
+            "data/tests/oai/openedition.xml").absolute().as_posix()
 
         self.from_date = datetime.datetime(2023, 10, 9)
         self.until_date = datetime.datetime(2023, 10, 10)
-        self.articles = []
+
+        self.header = None
+        self.record = None
 
         archive = ArchiveOAIDC(self.baseurl,
                                local_file=True,
@@ -41,25 +44,34 @@ class Openedition(unittest.TestCase):
 
         for i, conv in enumerate(archive.converters):
             conv.run()
-            self.articles += conv.articles
+
+            self.header = conv.header
+            self.record = conv.record
+
+        self.article = OAIArtcile_Openedition(self.header, self.record)
 
     def test_interface(self):
         """"""
-        for article in self.articles:
-            self.assertIn(IArticle, providedBy(article), "IArticle")
-            self.assertIsInstance(
-                article, OAIArtcile_Openedition, "Openedition Artikel")
+        self.assertIn(IArticle, providedBy(self.article), "IArticle")
+        self.assertIsInstance(self.article,
+                              OAIArtcile_Openedition,
+                              "Openedition Artikel")
 
 
 class OpeneditionValidate(unittest.TestCase):
 
     def setUp(self) -> None:
         self.baseurl = Path(
-            "data/tests/oai/response.xml").absolute().as_posix()
+            "data/tests/oai/openedition.xml").absolute().as_posix()
+        self.jpath = Path("data/tests/oai/article_openedition.json")
+
+        with open(self.jpath) as fh:
+            self.testdata = json.load(fh)
 
         self.from_date = datetime.datetime(2023, 10, 9)
         self.until_date = datetime.datetime(2023, 10, 10)
-        self.articles = []
+        self.header = None
+        self.record = None
 
         archive = ArchiveOAIDC(self.baseurl,
                                local_file=True,
@@ -69,14 +81,53 @@ class OpeneditionValidate(unittest.TestCase):
                                                  "validate": True})
 
         for i, conv in enumerate(archive.converters):
-            conv.run()
-            self.articles += conv.articles
+            self.header = conv.header
+            self.record = conv.record
+
+        self.article = OAIArtcile_Openedition(self.header, self.record)
+
+    def test_abstracts(self):
+        """"""
+        self.assertEqual(self.article.abstracts,
+                         self.testdata["abstracts"], "abstracts")
+
+    def test_dateOfProduction(self):
+        """"""
+        self.assertEqual(self.article.date_of_production,
+                         self.testdata["dateOfProduction"], "dateOfProduction")
+
+    def test_lang_code(self):
+        """"""
+        self.assertEqual(self.article.lang_code,
+                         self.testdata["lang_code"], "lang_code")
 
     def test_interface(self):
         """"""
-        from pprint import pprint
-        for article in self.articles:
-            self.assertIn(IArticle, providedBy(article), "IArticle")
-            self.assertIsInstance(
-                article, OAIArtcile_Openedition, "Openedition Artikel")
-            # pprint(article.jdict)
+        self.assertIn(IArticle, providedBy(self.article), "IArticle")
+        self.assertIsInstance(self.article,
+                              OAIArtcile_Openedition,
+                              "Openedition Artikel")
+
+    def test_journal(self):
+        """"""
+        self.assertEqual(self.article.journal,
+                         self.testdata["journal"], "journal")
+
+    def test_persons(self):
+        """"""
+        self.assertEqual(self.article.persons,
+                         self.testdata["persons"], "persons")
+
+    def test_primary_id(self):
+        """"""
+        self.assertEqual(self.article.primary_id,
+                         self.testdata["primary_id"], "primary_id")
+
+    def test_subject_terms(self):
+        """"""
+        self.assertEqual(self.article.subject_terms,
+                         self.testdata["subject_terms"], "subject_terms")
+
+    def test_title(self):
+        """"""
+        self.assertEqual(self.article.title, self.testdata["title"], "title")
